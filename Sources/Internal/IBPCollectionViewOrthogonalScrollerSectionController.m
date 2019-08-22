d2ccb2ea2d9ba282f5d8cbc8a0b4d10eeb6b1258
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2019-08-23 01:22:57 +0900

    Workaround reusing cell of orthogonal scrolling collection view

--- Sources/Internal/IBPCollectionViewOrthogonalScrollerSectionController.m
+++ Sources/Internal/IBPCollectionViewOrthogonalScrollerSectionController.m
@@ -33,7 +33,45 @@
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return [self.collectionView.dataSource collectionView:self.collectionView cellForItemAtIndexPath:indexPath];
+    NSDictionary *cellClassDict = @{};
+    @try {
+        cellClassDict = [[self.collectionView valueForKey:@"cellClassDict"] copy];
+    } @catch (NSException *exception) {}
+    for (NSString *reuseIdentifier in cellClassDict) {
+        Class cellClass = cellClassDict[reuseIdentifier];
+        [collectionView registerClass:cellClass forCellWithReuseIdentifier:reuseIdentifier];
+    }
+    NSDictionary *cellNibDict = @{};
+    @try {
+        cellNibDict = [[self.collectionView valueForKey:@"cellNibDict"] copy];
+    } @catch (NSException *exception) {}
+    for (NSString *reuseIdentifier in cellNibDict) {
+        UINib *cellNib = cellNibDict[reuseIdentifier];
+        [collectionView registerNib:cellNib forCellWithReuseIdentifier:reuseIdentifier];
+    }
+
+    return [self.collectionView.dataSource collectionView:collectionView cellForItemAtIndexPath:indexPath];
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *classDict = @{};
+    @try {
+        classDict = [[self.collectionView valueForKey:@"supplementaryViewClassDict"] copy];
+    } @catch (NSException *exception) {}
+    for (NSString *reuseIdentifier in classDict) {
+        Class viewClass = classDict[reuseIdentifier];
+        [collectionView registerClass:viewClass forSupplementaryViewOfKind:kind withReuseIdentifier:reuseIdentifier];
+    }
+    NSDictionary *nibDict = @{};
+    @try {
+        nibDict = [[self.collectionView valueForKey:@"supplementaryViewNibDict"] copy];
+    } @catch (NSException *exception) {}
+    for (NSString *reuseIdentifier in nibDict) {
+        UINib *nib = nibDict[reuseIdentifier];
+        [collectionView registerNib:nib forSupplementaryViewOfKind:kind withReuseIdentifier:reuseIdentifier];
+    }
+
+    return [self.collectionView.dataSource collectionView:collectionView viewForSupplementaryElementOfKind:kind atIndexPath:indexPath];
 }
 
 - (void)removeFromSuperview {
