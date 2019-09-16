3fb64b4cc9cf75d12402003ebb50bf2e3027d480
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2019-09-17 00:16:06 +0900

    Fix example UIColor extension

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -15,16 +15,16 @@ extension UIColor {
         return lightColor
     }
     static var placeholderText: UIColor {
-        let lightColor = UIColor(red: 235, green: 235, blue: 245, alpha: 0.3)
-        let darkColor = UIColor(red: 60, green: 60, blue: 67, alpha: 0.3)
+        let lightColor = UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.3)
+        let darkColor = UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.3)
         if #available(iOS 13.0, *) {
             return UIColor { $0.userInterfaceStyle == .light ? lightColor : darkColor }
         }
         return lightColor
     }
     static var systemGray2: UIColor {
-        let lightColor = UIColor(red: 174, green: 174, blue: 178, alpha: 1)
-        let darkColor = UIColor(red: 99, green: 99, blue: 102, alpha: 1)
+        let lightColor = UIColor(red: 174/255, green: 174/255, blue: 178/255, alpha: 1)
+        let darkColor = UIColor(red: 99/255, green: 99/255, blue: 102/255, alpha: 1)
         if #available(iOS 13.0, *) {
             return UIColor { $0.userInterfaceStyle == .light ? lightColor : darkColor }
         }
